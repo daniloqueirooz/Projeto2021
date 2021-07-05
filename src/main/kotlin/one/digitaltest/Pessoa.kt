53cf9b5d3ba79e82ea75f3f5b07f214173eff2ab
dqueirozp <danilodequeirozq@gmail.com>
2021-07-05 17:59:07 -0300

    update to apply the private modifier to set function of property

--- src/main/kotlin/one/digitaltest/Pessoa.kt
+++ src/main/kotlin/one/digitaltest/Pessoa.kt
@@ -2,13 +2,16 @@ package one.digitaltest
 
 class Pessoa {
     var name: String  = "Danilo"
+
     var cpf: String = "123.123.112-81"
+    private set
+    }
 
 
-
-}
 fun main (){
     val danilo = Pessoa()
+
+    println(danilo)
     println(danilo.name)
     println(danilo.cpf)
-}
+    }
