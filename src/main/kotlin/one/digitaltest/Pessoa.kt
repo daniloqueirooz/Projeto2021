73ca1671213c59c0c47e40e34076cb58620d985b
dqueirozp <danilodequeirozq@gmail.com>
2021-07-05 18:22:11 -0300

    update to talk about class members

--- src/main/kotlin/one/digitaltest/Pessoa.kt
+++ src/main/kotlin/one/digitaltest/Pessoa.kt
@@ -2,16 +2,16 @@ package one.digitaltest
 
 class Pessoa {
     var name: String  = "Danilo"
-
     var cpf: String = "123.123.112-81"
     private set
+
+    constructor()
+    fun pessoaInfo() ="$name + $cpf"
     }
 
 
-fun main (){
+fun main () {
     val danilo = Pessoa()
 
-    println(danilo)
-    println(danilo.name)
-    println(danilo.cpf)
-    }
+    println(danilo.pessoaInfo())
+}
