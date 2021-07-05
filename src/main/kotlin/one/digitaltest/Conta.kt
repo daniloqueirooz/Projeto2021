663e15131f62c78adb84ce5cf0447484d590f7a9
dqueirozp <danilodequeirozq@gmail.com>
2021-07-05 19:06:49 -0300

    creat acount class

---
+++ src/main/kotlin/one/digitaltest/Conta.kt
@@ -0,0 +1,21 @@
+package one.digitaltest
+
+import java.math.BigDecimal
+import javax.lang.model.element.NestingKind
+
+class Conta (
+    val agencia: String,
+    val numero: NestingKind,
+    val saldo: BigDecimal
+)
+
+fun deposito(valor: BigDecimal) {
+
+}
+     fun saque(valor: BigDecimal){
+
+     }
+
+
+
+
