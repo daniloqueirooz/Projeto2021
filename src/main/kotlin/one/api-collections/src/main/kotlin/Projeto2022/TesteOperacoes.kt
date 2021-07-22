db5dc9d9ac11819ac5bc52515252e89c652517b9
dqueirozp <danilodequeirozq@gmail.com>
2021-07-22 15:17:59 -0300

    creat array = max, min average e filter

---
+++ src/main/kotlin/one/api-collections/src/main/kotlin/Projeto2022/TesteOperacoes.kt
@@ -0,0 +1,19 @@
+package Projeto2022
+
+fun main () {
+    val salarios = doubleArrayOf(1000.0, 1500.0, 3000.0)
+
+    for (salario in salarios) {
+        println(salario)
+    }
+    println("-----------------")
+    println("Maior salario: ${salarios.maxOrNull()}")
+    println("Menor salario: ${salarios.minOrNull()}")
+    println("Media salarial: ${salarios.average()}")
+
+    println("-------------------------------")
+
+    val salarioMaiorQue2500 = salarios.filter { it > 2500 }
+    println("--------------------------")
+    salarioMaiorQue2500.forEach { println(it) }
+}
